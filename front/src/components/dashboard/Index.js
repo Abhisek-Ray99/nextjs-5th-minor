00c2df9c0c6ba8ba8ee4d15d59d8fdf6ead281f9
Abhisek-Ray99 <rickyrayabhisek99@gmail.com>
2021-10-10 11:30:45 +0530

    add visualizer1

--- front/src/components/dashboard/Index.js
+++ front/src/components/dashboard/Index.js
@@ -3,6 +3,8 @@ import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 
+import Visualizer1 from './visualizer1/Visualizer1'
+
 const user = {
   name: 'Tom Cook',
   email: 'tom@example.com',
@@ -184,10 +186,11 @@ export default function Example() {
         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
           {/* Replace with your content */}
 
-          <div className="px-4 py-6 sm:px-0">
+          {/* <div className="px-4 py-6 sm:px-0">
             <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
-          </div>
+          </div> */}
           {/* /End replace */}
+          <Visualizer1/>
         </div>
       </main>
     </div>
---
+++ front/src/components/dashboard/visualizer1/Visualizer1.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Cards from '../../Cards/Cards'
+import Chart from "../../Chart/Chart";
+import CountryPicker from "../../CountryPicker/CountryPicker";
+import styles from "./Visualizer1.module.css";
+import { fetchData } from "../../../api/index";
+
+class Visualizer1 extends React.Component {
+    state = {
+        data: {},
+        country: "",
+    };
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+        this.setState({ data: fetchedData });
+    }
+    handleCountryChange = async (country) => {
+        const fetchedData = await fetchData(country);
+        this.setState({ data: fetchedData, country: country });
+    };
+    render() {
+        const { data, country } = this.state;
+        return (
+            <div className={styles.container}>
+                <br />
+                <text>
+                    <b>Global and Country Wise Cases of Corona Virus</b>
+                </text>
+                <br />
+                <text>
+                    <i>(For a particular select a Country from below)</i>
+                </text>
+                <br />
+                <br />
+                <Cards data={data} country={country} />
+                <CountryPicker handleCountryChange={this.handleCountryChange} />
+                <Chart data={data} country={country} />
+            </div>
+        );
+    }
+}
+
+export default Visualizer1;
