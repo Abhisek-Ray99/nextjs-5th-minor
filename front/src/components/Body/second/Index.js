be3fd3c0a489cc9739a64e8b42031f6a83311b00
satyam7479 <sk6005848@gmail.com>
2021-11-03 02:14:04 +0530

    status

--- front/src/components/Body/second/Index.js
+++ front/src/components/Body/second/Index.js
@@ -9,17 +9,16 @@ import { Div, Status, Sub, User, Time, Icon } from './Secondelement';
 const Second = () => {
     return (
         <Div>
-            <h1>Let's Gtok Features</h1>
+            <h1>Let's Covid-Visualizer Features</h1>
             <div className="main">
                     <div>
                         <Status>
-                            <span>Current Feeling</span>
                             <IconContext.Provider value={{className: "more"}}>
                                 <FiMoreHorizontal />
                             </IconContext.Provider>
                         </Status>
-                        <Sub>So excited to finally get my hands on the latest ps5</Sub>
-                        <User>@josh</User>
+                        <Sub>It's easy to use and a nice representation...</Sub>
+                        <User>@rahul</User>
                         <Time>a few seconds ago</Time>
                         <Icon>
                             <IconContext.Provider value={{className: "icon1"}}>
@@ -32,14 +31,13 @@ const Second = () => {
                     </div>
                     <div>
                         <Status>
-                            <span>Watching</span>
                             <IconContext.Provider value={{className: "more"}}>
                                 <FiMoreHorizontal />
                             </IconContext.Provider>
                         </Status>
-                        <Sub>So excited to finally get my hands on the latest ps5</Sub>
-                        <User>@josh</User>
-                        <Time>a few seconds ago</Time>
+                        <Sub>i most liked his shared feelings....after reading that somewhat my stress was gone for covid.</Sub>
+                        <User>@manvya</User>
+                        <Time>4 hrs ago</Time>
                         <Icon>
                             <IconContext.Provider value={{className: "icon1"}}>
                                 <AiFillHeart />
@@ -51,14 +49,13 @@ const Second = () => {
                     </div>
                     <div>
                         <Status>
-                            <span>Watching</span>
                             <IconContext.Provider value={{className: "more"}}>
                                 <FiMoreHorizontal />
                             </IconContext.Provider>
                         </Status>
-                        <Sub>So excited to finally get my hands on the latest ps5</Sub>
+                        <Sub>Dashboard is easy to use</Sub>
                         <User>@josh</User>
-                        <Time>a few seconds ago</Time>
+                        <Time>1 day ago</Time>
                         <Icon>
                             <IconContext.Provider value={{className: "icon1"}}>
                                 <AiFillHeart />
@@ -70,14 +67,13 @@ const Second = () => {
                     </div>
                     <div>
                         <Status>
-                            <span>Watching</span>
                             <IconContext.Provider value={{className: "more"}}>
                                 <FiMoreHorizontal />
                             </IconContext.Provider>
                         </Status>
-                        <Sub>So excited to finally get my hands on the latest ps5</Sub>
-                        <User>@josh</User>
-                        <Time>a few seconds ago</Time>
+                        <Sub>graphical representation is easy to understand and also looking attractive.</Sub>
+                        <User>@ricky</User>
+                        <Time>2 days ago</Time>
                         <Icon>
                             <IconContext.Provider value={{className: "icon1"}}>
                                 <AiFillHeart />
